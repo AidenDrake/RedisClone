749e0129d9f3e00816790c4a9317fd3570c4e151
Aiden <aidenndrake@gmail.com>
2025-03-27 19:01:23 -0400

    basic networking

--- RedisClone/Program.cs
+++ RedisClone/Program.cs
@@ -1,8 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
-Console.WriteLine("Hello world");
+var listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
+listener.Blocking = true;
+var endpoint = new IPEndPoint(IPAddress.Any, 6379);
+listener.Bind(endpoint);
+listener.Listen(100);
+var handler = listener.Accept();
+
+while (true)
+{
+    var buffer = new byte[1024];
+    var received = handler.Receive(buffer, SocketFlags.None);
+
+    var response = Encoding.ASCII.GetString(buffer, 0, received);
+    if (response.Length > 0)
+    {
+        Console.WriteLine($"Received {response}");
+        break;
+    }
+}
+
 
 namespace RedisClone
 {
